fe0faf37a4f0b0faf1ff4272c3dcaa787682915f
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-11-29 13:28:24 -0500

    Move data download to jworkflow package

---
+++ jworkflow/data.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+from urllib.request import urlretrieve
+
+FREMONT_URL = "https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD"
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+	"""Download and cache the fremont data
+
+	Paramters
+	---------
+	filename : string (optional)
+		location to save data
+	url : string (optional)
+		web location of data
+	force_download : bool (optional)
+		if True, force redownload of data
+
+	Returns
+	-------
+	data : pandas.DataFrame
+		Fremont bridge data
+	"""
+
+	if not os.path.exists(filename) or force_download:
+		urlretrieve(url, filename)
+	data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+	data.columns = ['West', 'East']
+	data['Total'] = data['West'] + data['East']
+	return data
