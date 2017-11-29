4384374909cea8495fe3f7c3598f3042624c0b89
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-11-29 14:29:16 -0500

    BUG: fix bug in date parsing

--- jworkflow/tests/test_data.py
+++ jworkflow/tests/test_data.py
@@ -1,5 +1,6 @@
-from jworkflow.data import get_fremont_data 
-import pandas as pd 
+from jworkflow.data import get_fremont_data
+import pandas as pd
+import numpy as np
 
 def test_fremont_data():
     data = get_fremont_data()
@@ -7,3 +8,5 @@ def test_fremont_data():
     assert all(data.columns == ['West', 'East', 'Total'])
     # also want date.index to be a DateTimeIndex instance
     assert isinstance(data.index, pd.DatetimeIndex)
+    # make sure data has 12-hour columns: check for 24 unique hours (?)
+    assert len(np.unique(data.index.time) == 24)
